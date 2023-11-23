ab48c2b3e9d84c9b3d98bc236b9305b417f43993
Joel Davidson <joeldavo97@gmail.com>
2023-11-23 16:34:04 +0000

    Added data models

---
+++ NutritionAPI/Models/FoodGroup.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NutritionAPI.Models;
+
+public class FoodGroup
+{
+    [Key]
+    public string FoodGroupCode { get; set; }
+    public string Description { get; set; }
+    public ICollection<FoodItem> FoodItems { get; set; }
+}
---
+++ NutritionAPI/Models/FoodItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NutritionAPI.Models;
+
+public class FoodItem
+{
+    [Key]
+    public string FoodCode { get; set; }
+    public string Name { get; set; }
+    public string FoodGroupCode { get; set; }
+    public FoodGroup FoodGroup { get; set; }
+    public MacronutrientsAndEnergy MacronutrientsAndEnergy { get; set; }
+    public Minerals Minerals { get; set; }
+    public Vitamins Vitamins { get; set; }
+}
