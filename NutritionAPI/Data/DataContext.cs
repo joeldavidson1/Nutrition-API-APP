469718a65d7a6007b71ae84ff29da295c1557681
Joel Davidson <joeldavo97@gmail.com>
2023-11-23 16:37:32 +0000

    Added DataContext to match up tables with PostgreSQL

---
+++ NutritionAPI/Data/DataContext.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using NutritionAPI.Models;
+
+namespace NutritionAPI.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<FoodItem> FoodItems { get; set; }
+    public DbSet<FoodGroup> FoodGroups { get; set; }
+    public DbSet<MacronutrientsAndEnergy> MacronutrientsAndEnergy { get; set; }
+    public DbSet<Minerals> Minerals { get; set; }
+    public DbSet<Vitamins> Vitamins { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // modelBuilder.Entity<FoodItem>().ToTable("fooditems"); // Ensure correct table name
+
+        modelBuilder.Entity<FoodItem>(entity =>
+        {
+            entity.HasOne(foodItem => foodItem.FoodGroup)
+                .WithMany(foodGroup => foodGroup.FoodItems)
+                .HasForeignKey(foodItem => foodItem.FoodGroupCode);
+        
+            entity.HasOne(m => m.MacronutrientsAndEnergy)
+                .WithOne(foodItem => foodItem.FoodItem)
+                .HasForeignKey<MacronutrientsAndEnergy>(x => x.FoodCode);
+        
+            entity.HasOne(m => m.Minerals)
+                .WithOne(foodItem => foodItem.FoodItem)
+                .HasForeignKey<Minerals>(x => x.FoodCode);
+            
+            entity.HasOne(v => v.Vitamins)
+                .WithOne(foodItem => foodItem.FoodItem)
+                .HasForeignKey<Vitamins>(x => x.FoodCode);
+        });
+    }
+}
