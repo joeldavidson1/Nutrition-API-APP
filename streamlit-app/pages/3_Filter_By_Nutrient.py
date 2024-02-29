a286500b0b510aac1d3b2e010fc08f86fc96b1bb
Joel Davidson <joeldavo97@gmail.com>
2024-02-29 07:15:56 +0000

    Added option to select food when filtering food via a nutrient

--- streamlit-app/pages/3_Filter_By_Nutrient.py
+++ streamlit-app/pages/3_Filter_By_Nutrient.py
@@ -4,6 +4,7 @@ import constants
 import api_handler
 import credentials
 import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 
 # Initialize session state variables
@@ -55,7 +56,107 @@ with st.container():
         for food in response:
             nutrient_data[food["Name"]] = food[constants.NUTRIENTS_TO_CATEGORIES.get(selected_option)][formatted_option]
 
-        df = pd.DataFrame({"Food": list(nutrient_data.keys()), selected_option: list(nutrient_data.values())})
-        st.write(df)
+        df = pd.DataFrame(response)
+        # Create a new column in df for the selected option and its values
+        df[selected_option] = df.apply(lambda row: row[constants.NUTRIENTS_TO_CATEGORIES.get(selected_option)][formatted_option], axis=1)
+        df_to_display = df[['FoodCode', 'Name', selected_option]]
+        st.markdown("#### Select a food item to view more details")
+        gd = GridOptionsBuilder.from_dataframe(df_to_display)
+        gd.configure_pagination(enabled=True)
+        gd.configure_selection(selection_mode='single', use_checkbox=True)
+        grid_table = AgGrid(df, gridOptions=gd.build(), 
+                            height=500, width='100%', 
+                            theme='streamlit', 
+                            editable=False,
+                            fit_columns_on_grid_load=True,
+                            custom_css={
+                            "#gridToolBar": {
+                                "padding-bottom": "0px !important",
+                                }
+                                })
+
+        if (grid_table['selected_rows']):
+            selected_row = grid_table['selected_rows'][0]
+            # st.write(selected_row)
+            st.subheader(f'{selected_row['FoodCode']}: {selected_row['Name']}')
+            st.markdown(f"**Food Group**: {helper.get_key(food_groups_dict, selected_row['FoodGroupCode'])}")
+            st.markdown(f"**Description**: {selected_row['Description']}")
+            st.markdown(f"**Data References**: {selected_row['DataReferences']}")
+
+            # Initialize a list to hold the columns
+            columns = []
+
+            for i, (category, nutrients) in enumerate(selected_row.items()):
+                # Skip the keys that are not nutrient categories
+                if category not in constants.NUTRIENT_CATEGORIES:
+                    continue
+
+                # Create a new row every 2 categories
+                if i % 2 == 0:
+                    columns = st.columns(2)
+
+                # Convert the nutrients dictionary to a DataFrame
+                df = pd.DataFrame([(nutrient, value) for nutrient, value in nutrients.items()], columns=['Nutrient', 'Value'])
+
+                # Apply the reverse_format_nutrient_option method to the 'Nutrient' column
+                df['Nutrient'] = df['Nutrient'].apply(helper.reverse_format_nutrient_option)
+                
+                # Use the current column for the category subheader and DataFrame
+                with columns[i % 2]:
+                    st.subheader(f"{category}:\n")
+                    st.dataframe(df)
+        # st.write(df)
     else:
         st.write("No data available")
+
+
+#  df = pd.DataFrame(response)
+#     df_to_display = df[['FoodCode', 'Name']]
+#     # AgGrid(df_to_display, )
+#     # st.write(df_to_display)
+
+#     st.subheader("Select a food item to view more details")
+#     gd = GridOptionsBuilder.from_dataframe(df_to_display)
+#     gd.configure_pagination(enabled=True)
+#     gd.configure_selection(selection_mode='single', use_checkbox=True)
+#     grid_table = AgGrid(df, gridOptions=gd.build(), 
+#                         height=500, width='100%', 
+#                         theme='streamlit', 
+#                         editable=False,
+#                         fit_columns_on_grid_load=True,
+#                         custom_css={
+#                         "#gridToolBar": {
+#                             "padding-bottom": "0px !important",
+#                             }
+#                             })
+
+#     if (grid_table['selected_rows']):
+#         selected_row = grid_table['selected_rows'][0]
+#         # st.write(selected_row)
+#         st.subheader(f'{selected_row['FoodCode']}: {selected_row['Name']}')
+#         st.markdown(f"**Food Group**: {helper.get_key(food_groups_dict, selected_row['FoodGroupCode'])}")
+#         st.markdown(f"**Description**: {selected_row['Description']}")
+#         st.markdown(f"**Data References**: {selected_row['DataReferences']}")
+
+#     # Initialize a list to hold the columns
+#         columns = []
+
+#         for i, (category, nutrients) in enumerate(selected_row.items()):
+#             # Skip the keys that are not nutrient categories
+#             if category not in constants.NUTRIENT_CATEGORIES:
+#                 continue
+
+#             # Create a new row every 2 categories
+#             if i % 2 == 0:
+#                 columns = st.columns(2)
+
+#             # Convert the nutrients dictionary to a DataFrame
+#             df = pd.DataFrame([(nutrient, value) for nutrient, value in nutrients.items()], columns=['Nutrient', 'Value'])
+
+#             # Apply the reverse_format_nutrient_option method to the 'Nutrient' column
+#             df['Nutrient'] = df['Nutrient'].apply(helper.reverse_format_nutrient_option)
+            
+#             # Use the current column for the category subheader and DataFrame
+#             with columns[i % 2]:
+#                 st.subheader(f"{category}:\n")
+#                 st.dataframe(df)
--- streamlit-app/pages/4_Browse_All_Foods.py
+++ streamlit-app/pages/4_Browse_All_Foods.py
@@ -20,51 +20,11 @@ selected_group = st.selectbox("Filter by Food Group", food_groups)
 food_group_code = food_groups_dict[selected_group]
 response = api_handler.get_data_from_api(credentials.api_key, f"foodGroups/{food_group_code}/foodItems", True)
 
-df = pd.DataFrame(response)
-df_to_display = df[['FoodCode', 'Name']]
-# AgGrid(df_to_display, )
-# st.write(df_to_display)
+if response:
+    df = pd.DataFrame(response)
+    df_to_display = df[['FoodCode', 'Name']]
+    # AgGrid(df_to_display, )
+    # st.write(df_to_display)
 
-st.subheader("Select a food item to view more details")
-gd = GridOptionsBuilder.from_dataframe(df_to_display)
-gd.configure_pagination(enabled=True)
-gd.configure_selection(selection_mode='single', use_checkbox=True)
-grid_table = AgGrid(df, gridOptions=gd.build(), height=500, width='100%', theme='streamlit', editable=False,
-                     custom_css={
-                    "#gridToolBar": {
-                        "padding-bottom": "0px !important",
-                        }
-                        })
-
-if (grid_table['selected_rows']):
-    selected_row = grid_table['selected_rows'][0]
-    # st.write(selected_row)
-    st.subheader(f'{selected_row['FoodCode']}: {selected_row['Name']}')
-    st.markdown(f"**Food Group**: {helper.get_key(food_groups_dict, selected_row['FoodGroupCode'])}")
-    st.markdown(f"**Description**: {selected_row['Description']}")
-    st.markdown(f"**Data References**: {selected_row['DataReferences']}")
-
-   # Initialize a list to hold the columns
-    columns = []
-
-    for i, (category, nutrients) in enumerate(selected_row.items()):
-        # Skip the keys that are not nutrient categories
-        if category not in constants.NUTRIENT_CATEGORIES:
-            continue
-
-        # Create a new row every 2 categories
-        if i % 2 == 0:
-            columns = st.columns(2)
-
-        # Convert the nutrients dictionary to a DataFrame
-        df = pd.DataFrame([(nutrient, value) for nutrient, value in nutrients.items()], columns=['Nutrient', 'Value'])
-
-        # Apply the reverse_format_nutrient_option method to the 'Nutrient' column
-        df['Nutrient'] = df['Nutrient'].apply(helper.reverse_format_nutrient_option)
-        
-        # Use the current column for the category subheader and DataFrame
-        with columns[i % 2]:
-            st.subheader(f"{category}:\n")
-            st.dataframe(df)
-
-
+    st.subheader("Select a food item to view more details")
+    gd = GridOptionsBuilder.from_dataframe(df
